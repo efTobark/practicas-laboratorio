78adb3e73d43733c60e822e27c5c86596b9e1f2d
Eduardo Fuentes <efuentes.isc@gmail.com>
2025-05-17 14:09:31 -0600

    Archivo inicial para laboratorio 3

---
+++ LAB3-INICIAL.py
@@ -0,0 +1,13 @@
+# LABORATORIO 3
+
+lista_clientes = ["Ana Pérez", "Carlos López", "Sofía Gómez", "Pedro Vargas"]
+
+ofertas_semanales = {
+    "lunes": "20% de descuento en todos el pan integral",
+    "martes": "¡Compra 3 croissants de almendra y llévate el cuarto gratis!",
+    "miércoles": "Oferta especial en nuestras deliciosas magdalenas de chocolate",
+    "jueves": "Pan de masa madre recién horneado a precio especial",
+    "viernes": "¡No te pierdas nuestros rollos de canela recién horneados!"
+}
+
+dia_actual = "martes"
